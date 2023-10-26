deb7a7a08a42cffc1e8ca3f8d7be9ce9a94d486d
Dominic <dominicudousoro@gmail.com>
2023-10-26 15:56:01 +0000

    MRU Cache

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-"""Create a class LRUCache that inherits from
-BaseCaching and is a caching system:
-
-You must use self.cache_data - dictionary from
-the parent class BaseCaching
+"""Create a class MRUCache that inherits
+from BaseCaching 
 """
+
+
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class LRUCache(BaseCaching):
+class MRUCache(BaseCaching):
     """_summary_
     """
 
@@ -33,7 +32,7 @@ class LRUCache(BaseCaching):
                 self.usedKeys.append(
                     self.usedKeys.pop(self.usedKeys.index(key)))
             if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
-                discard = self.usedKeys.pop(0)
+                discard = self.usedKeys.pop(-2)
                 del self.cache_data[discard]
                 print('DISCARD: {:s}'.format(discard))
 
